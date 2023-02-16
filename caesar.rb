950690bd2432c3bad1a2353b1464913c43a69ee6
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2023-02-16 15:34:05 +0000

    Fix 'shift' to assign only an integer

--- caesar.rb
+++ caesar.rb
@@ -1,19 +1,22 @@
+
+
 #Asks for input until it doesn't contain numbers
 puts "What would you like to encrypt?"
+string = gets
 loop do
-  string = gets
-  split_word = string.split
-  unless split_word.any? {|element| element.to_i > 0}
+  unless string.split('').any? {|element| element.to_i > 0} 
     break
+  else
+    string = gets
   end
 end
 
-#Asks for input until it is an integer
+#Asks for the number of positions the letter are to be shifted
 puts "And what would the shifting factor be?"
-loop do
-  shift = gets.to_i
-  if shift > 0
-    break
-  end
-end
+shift = gets.to_i
 
+#alphabet = ('a'..'z').to_a
+#print alphabet
+
+def caesar_cipher (string, shift) 
+end
