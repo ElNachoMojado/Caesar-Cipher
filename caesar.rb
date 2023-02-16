15848a674546a3eee05ac640aec37e5c8d041d48
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2023-02-16 21:27:04 +0000

    Add caesar_cipher function to encrypt provided string

--- caesar.rb
+++ caesar.rb
@@ -1,5 +1,3 @@
-
-
 #Asks for input until it doesn't contain numbers
 puts "What would you like to encrypt?"
 string = gets
@@ -10,13 +8,27 @@ loop do
     string = gets
   end
 end
+string = string.split('').to_a
+puts string
 
 #Asks for the number of positions the letter are to be shifted
 puts "And what would the shifting factor be?"
 shift = gets.to_i
 
-#alphabet = ('a'..'z').to_a
-#print alphabet
-
-def caesar_cipher (string, shift) 
+def caesar_cipher (string, shift)
+  alphabet = ('a'..'z').to_a
+  caps_alphabet = ('A'..'Z').to_a
+  encrypted_word = []
+  for character in string do
+    alphabet.each_index do |index|
+      if character == alphabet[index]
+        encrypted_word.push(alphabet[index+shift])
+      elsif character == caps_alphabet[index]
+        encrypted_word.push(caps_alphabet[index+shift])
+      end
+    end
+  end
+  puts encrypted_word.join('')   
 end
+
+caesar_cipher(string, shift)
