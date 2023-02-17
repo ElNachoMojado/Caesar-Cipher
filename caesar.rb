b731ee9eee340026b3bb00b8bf83be06fa6d8661
Daniel <118195299+ElNachoMojado@users.noreply.github.com>
2023-02-17 20:36:25 +0000

    Fix array declaration so the encrypted message retains spaces between words/Remove exclamation mark

--- caesar.rb
+++ caesar.rb
@@ -1,3 +1,5 @@
+
+
 #Asks for input until it doesn't contain numbers
 puts "What would you like to encrypt?"
 string = gets
@@ -8,7 +10,8 @@ loop do
     string = gets
   end
 end
-string = string.split('').to_a
+string = string.split(' ').map {|word| word.split('')}.to_a
+
 
 #Asks for the number of positions the letter are to be shifted
 puts "And what would the shifting factor be?"
@@ -17,32 +20,31 @@ shift = gets.to_i
 def caesar_cipher (string, shift)
   alphabet = ('a'..'z').to_a
   caps_alphabet = ('A'..'Z').to_a
-  encrypted_word = []
+  encrypted_word = Array.new(string.length) {[]}
   wrap = 0
-  for character in string do
-  if character == 33.chr
-    encrypted_word.push(character)
-    next
-  end
-    alphabet.each_index do |index|
-      if character == alphabet[index]
-        if shift + index > 26
-          wrap = shift + index - 26
-          encrypted_word.push(alphabet[wrap])
-        else
-          encrypted_word.push(alphabet[shift + index])
-        end
-      elsif character == caps_alphabet[index]
-        if shift + index > 26
-          wrap = shift + index - 26
-          encrypted_word.push(caps_alphabet[wrap])
-        else
-          encrypted_word.push(caps_alphabet[index+shift])
+  string.each_index do |word_index|
+    string[word_index].each_index do |letter_index|
+      alphabet.each_index do |index|
+        if string[word_index][letter_index] == alphabet[index]
+          if shift + index > 26
+            wrap = shift + index - 26
+            encrypted_word[word_index].push(alphabet[wrap])
+          else
+            encrypted_word[word_index].push(alphabet[shift + index])
+          end
+        elsif string[word_index][letter_index] == caps_alphabet[index]
+          if shift + index > 26
+            wrap = shift + index - 26
+            encrypted_word[word_index].push(caps_alphabet[wrap])
+          else
+            encrypted_word[word_index].push(caps_alphabet[shift + index])
+          end
         end
       end
     end
   end
-  puts encrypted_word.join('') 
+  puts encrypted_word.map {|letters| letters.join('')}.join(' ')
+  
 end
 
 caesar_cipher(string, shift)
